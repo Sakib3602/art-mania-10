cca9fc29a2ac88068379a6491835c83c2042348b
Sakib3602 <sakibsarker6969@gmail.com>
2024-04-28 23:40:34 +0600

    added img in slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,69 +1,108 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 // import Swiper styles
-import 'swiper/css';
+import "swiper/css";
 
 // import required modules
-import {  Autoplay,Grid, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Grid, Pagination, Navigation } from "swiper/modules";
 
 const Slider = () => {
-    const [text] = useTypewriter({
-        words: ['Art','Craft','Transform','Sculpt','Imagine'],
-        loop: 3,
-        onLoopDone: () => console.log(`loop completed after 3 runs.`)
-      })
-    return (
-        <>
-        <h1 className='text-[30px] lg:text-[40px] md:text-[40px] font-[700]'>Choose Your Dream {text} <Cursor cursorColor='red' /></h1>
+  const [text] = useTypewriter({
+    words: ["Art", "Craft", "Transform", "Sculpt", "Imagine"],
+    loop: 3,
+    onLoopDone: () => console.log(`loop completed after 3 runs.`),
+  });
+  return (
+    <>
+      <h1 className="text-[30px] lg:text-[40px] md:text-[40px] font-[700]">
+        Choose Your Dream {text} <Cursor cursorColor="red" />
+      </h1>
       <Swiper
         slidesPerView={3}
-        
         spaceBetween={10}
         autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Autoplay,Grid, Pagination, Navigation]}
-        
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Grid, Pagination, Navigation]}
         className="mySwiper h-[200px] lg:h-[400px]"
       >
-        <SwiperSlide className='   mt-5' >
-            <img src="https://images.unsplash.com/photo-1612111819547-ac62f91b1a94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGV4dGlsZSUyMGFydHxlbnwwfHwwfHx8MA%3D%3D" className='rounded-xl w-full h-full' alt="" />
+        <SwiperSlide className="   mt-5">
+          <img
+            src="https://i.ibb.co/7vx8Vgq/mike-petrucci-f6-Xv0xs9-JWg-unsplash.jpg"
+            className="rounded-xl w-full h-full"
+            alt=""
+          />
         </SwiperSlide>
-        <SwiperSlide className='  mt-5' >
-            <img src="https://images.unsplash.com/photo-1612111819547-ac62f91b1a94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGV4dGlsZSUyMGFydHxlbnwwfHwwfHx8MA%3D%3D" className='rounded-xl w-full h-full' alt="" />
+        <SwiperSlide className="  mt-5">
+          <img
+            src="
+https://i.ibb.co/9tTyBVD/amauri-mejia-Ih-Xr-WDck-ZOQ-unsplash.jpg"
+            className="rounded-xl w-full h-full"
+            alt=""
+          />
         </SwiperSlide>
-        <SwiperSlide className=' mt-5' >
-            <img src="https://images.unsplash.com/photo-1612111819547-ac62f91b1a94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGV4dGlsZSUyMGFydHxlbnwwfHwwfHx8MA%3D%3D" className='rounded-xl w-full h-full' alt="" />
+        <SwiperSlide className=" mt-5">
+          <img
+            src="https://i.ibb.co/d0Z8XYY/billow926-c-GEed-HDzlj-M-unsplash.jpg"
+            className="rounded-xl w-full h-full"
+            alt=""
+          />
         </SwiperSlide>
-        <SwiperSlide className=' mt-5' >
-            <img src="https://images.unsplash.com/photo-1612111819547-ac62f91b1a94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGV4dGlsZSUyMGFydHxlbnwwfHwwfHx8MA%3D%3D" className='rounded-xl w-full h-full' alt="" />
+        <SwiperSlide className=" mt-5">
+          <img
+            src="
+https://i.ibb.co/vk7NFRS/unseen-studio-s9-CC2-SKy-SJM-unsplash.jpg"
+            className="rounded-xl w-full h-full"
+            alt=""
+          />
         </SwiperSlide>
-        <SwiperSlide className='  mt-5' >
-            <img src="https://images.unsplash.com/photo-1612111819547-ac62f91b1a94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGV4dGlsZSUyMGFydHxlbnwwfHwwfHx8MA%3D%3D" className='rounded-xl w-full h-full' alt="" />
+        <SwiperSlide className="  mt-5">
+          <img
+            src="
+https://i.ibb.co/tqSbb4G/tim-marshall-c-Atz-HUz7-Z8g-unsplash.jpg"
+            className="rounded-xl w-full h-full"
+            alt=""
+          />
         </SwiperSlide>
-        <SwiperSlide className=' mt-5' >
-            <img src="https://images.unsplash.com/photo-1612111819547-ac62f91b1a94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGV4dGlsZSUyMGFydHxlbnwwfHwwfHx8MA%3D%3D" className='rounded-xl w-full h-full' alt="" />
+        <SwiperSlide className=" mt-5">
+          <img
+            src="https://i.ibb.co/j60LcV9/khara-woods-KR84-Rp-MCb0w-unsplash.jpg"
+            className="rounded-xl w-full h-full"
+            alt=""
+          />
         </SwiperSlide>
-        <SwiperSlide className='  mt-5' >
-            <img src="https://images.unsplash.com/photo-1612111819547-ac62f91b1a94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGV4dGlsZSUyMGFydHxlbnwwfHwwfHx8MA%3D%3D" className='rounded-xl w-full h-full' alt="" />
+        <SwiperSlide className="  mt-5">
+          <img
+            src="https://i.ibb.co/nrqWMZK/russn-fckr-kr-V5a-S4j-Dj-A-unsplash.jpg"
+            className="rounded-xl w-full h-full"
+            alt=""
+          />
         </SwiperSlide>
-        <SwiperSlide className='  mt-5' >
-            <img src="https://images.unsplash.com/photo-1612111819547-ac62f91b1a94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGV4dGlsZSUyMGFydHxlbnwwfHwwfHx8MA%3D%3D" className='rounded-xl w-full h-full' alt="" />
+        <SwiperSlide className="  mt-5">
+          <img
+            src="https://i.ibb.co/41hGnkd/alice-donovan-rouse-yu68f-UQDv-OI-unsplash.jpg"
+            className="rounded-xl w-full h-full"
+            alt=""
+          />
         </SwiperSlide>
-        <SwiperSlide className='  mt-5' >
-            <img src="https://images.unsplash.com/photo-1612111819547-ac62f91b1a94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGV4dGlsZSUyMGFydHxlbnwwfHwwfHx8MA%3D%3D" className='rounded-xl w-full h-full' alt="" />
+        <SwiperSlide className="  mt-5">
+          <img
+            src="https://i.ibb.co/XD490z9/andrian-valeanu-yj-Xlyr-KIz2-A-unsplash.jpg"
+            className="rounded-xl w-full h-full"
+            alt=""
+          />
         </SwiperSlide>
       </Swiper>
     </>
-    );
+  );
 };
 
 export default Slider;
